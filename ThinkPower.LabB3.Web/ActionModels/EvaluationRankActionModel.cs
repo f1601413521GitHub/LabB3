ddab3b3ff6bf061c6f42334608fa0681ed3418d1
JasonLin <f1601413521@outlook.com>
2018-08-21 20:21:11 +0800

    調整 WEB 專案 1. 調整 投資風險評估問卷填答 來源資料類別 屬性欄位 2. 調整 投資風險評估 作業     1. 新增 NLog 功能     2. 新增 投資風險評估服務 欄位     3. 調整 進行投資風險評估問卷填答 功能

--- ThinkPower.LabB3.Web/ActionModels/EvaluationRankActionModel.cs
+++ ThinkPower.LabB3.Web/ActionModels/EvaluationRankActionModel.cs
@@ -1,6 +1,15 @@
 ﻿namespace ThinkPower.LabB3.Web.ActionModels
 {
+    /// <summary>
+    /// 投資風險評估問卷填答 來源資料類別
+    /// </summary>
     public class EvaluationRankActionModel
     {
+        /// <summary>
+        /// 問卷編號
+        /// 問卷1:FNDRE001
+        /// 問卷2:FNDRE002
+        /// </summary>
+        public string questId { get; internal set; }
     }
 }
--- ThinkPower.LabB3.Web/Controllers/RiskEvaluationController.cs
+++ ThinkPower.LabB3.Web/Controllers/RiskEvaluationController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThinkPower.LabB3.Domain.Entity.Risk;
+using ThinkPower.LabB3.Domain.Service;
 using ThinkPower.LabB3.Web.ActionModels;
 
 namespace ThinkPower.LabB3.Web.Controllers
@@ -12,6 +15,21 @@ namespace ThinkPower.LabB3.Web.Controllers
     /// </summary>
     public class RiskEvaluationController : Controller
     {
+        /// <summary>
+        /// NLog Object
+        /// </summary>
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// 投資風險評估服務
+        /// </summary>
+        private RiskEvaluationService _riskService;
+
+        public RiskEvaluationController()
+        {
+            _riskService = new RiskEvaluationService();
+        }
+
         /// <summary>
         /// 確認接受投資風險評估結果
         /// </summary>
@@ -45,7 +63,21 @@ namespace ThinkPower.LabB3.Web.Controllers
         public ActionResult EvaQuest(EvaluationRankActionModel actionModel)
         {
             //TODO EvaQuest 進行投資風險評估問卷填答
-            return View();
+            RiskEvaQuestionnaireEntity result = new RiskEvaQuestionnaireEntity();
+
+            try
+            {
+                if (actionModel != null)
+                {
+                    result = _riskService.GetRiskQuestionnaire(actionModel.questId);
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+            }
+
+            return View(result);
         }
     }
 }
