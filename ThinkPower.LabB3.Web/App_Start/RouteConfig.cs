cdb93cfb287993f1e16c3be4e6c241c71f32e023
JasonLin <f1601413521@outlook.com>
2018-09-17 10:27:53 +0800

    調整Layout樣式設定與RouteConfig預設值。

--- ThinkPower.LabB3.Web/App_Start/RouteConfig.cs
+++ ThinkPower.LabB3.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ThinkPower.LabB3.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "EvaQuest", id = UrlParameter.Optional }
             );
         }
     }
