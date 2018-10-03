dace92b4f270fc9e873104145d50de9fc21b798e
JasonLin <f1601413521@outlook.com>
2018-10-03 21:09:05 +0800

    新增投資風險等級明細類別。

---
+++ ThinkPower.LabB3.Domain/Entity/RiskRankDetailEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace ThinkPower.LabB3.Domain.Entity
+{
+    /// <summary>
+    /// 投資風險等級明細類別
+    /// </summary>
+    public class RiskRankDetailEntity : BaseEntity
+    {
+
+        /// <summary> 
+        ///紀錄識別碼 
+        /// </summary> 
+        public System.Guid RiskRankUid { get; set; }
+
+        /// <summary> 
+        ///投資標的風險收益等級 
+        /// </summary> 
+        public string ProfitRiskRank { get; set; }
+
+        /// <summary> 
+        ///是否有效 
+        /// </summary> 
+        public string IsEffective { get; set; }
+
+    }
+}
