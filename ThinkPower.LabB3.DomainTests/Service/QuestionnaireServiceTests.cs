422d166764918c95599c2563fcb57380240dfed4
JasonLin <f1601413521@outlook.com>
2018-10-09 18:09:36 +0800

    修正QuestionnaireService，需填寫其他說明文字的單元測試案例。

--- ThinkPower.LabB3.DomainTests/Service/QuestionnaireServiceTests.cs
+++ ThinkPower.LabB3.DomainTests/Service/QuestionnaireServiceTests.cs
@@ -96,7 +96,7 @@ namespace ThinkPower.LabB3.Domain.Service.Tests
                 }
             };
         }
-        
+
         [TestMethod()]
         public void CalculateScoreTest_When_CountScoreTypeIsMax_ScoreIs19_Then_Success()
         {
@@ -421,12 +421,70 @@ namespace ThinkPower.LabB3.Domain.Service.Tests
         public void ValidateRule_When_OtherAnswer_Then_Fail()
         {
             //Arrange
-            var answer = QuestionAnswerEntity();
-            var question = QuestionEntity();
-            answer.AnswerDetailEntities.First(x => x.QuestionId == "Q003").AnswerCode = "6";
+            var answer = new QuestionnaireAnswerEntity()
+            {
+                AnswerDetailEntities = new List<AnswerDetailEntity>()
+                {
+                    new AnswerDetailEntity() {QuestionId="Q1", AnswerCode = "A", OtherAnswer = "", Score = null, },
+                    new AnswerDetailEntity() {QuestionId="Q2", AnswerCode = "A", OtherAnswer = "", Score = null, },
+                    new AnswerDetailEntity() {QuestionId="Q3", AnswerCode = "A", OtherAnswer = "", Score = null, },
+                    new AnswerDetailEntity() {QuestionId="Q3", AnswerCode = "E", OtherAnswer = "", Score = null, },
+                    new AnswerDetailEntity() {QuestionId="Q4", AnswerCode = "A", OtherAnswer = "", Score = null, },
+                    new AnswerDetailEntity() {QuestionId="Q5", AnswerCode = "A", OtherAnswer = "", Score = null, },
+                },
+            };
+
+            var question = new QuestionnaireEntity()
+            {
+                ScoreKind = "",
+                QuestScore = null,
+                NeedScore = "N",
+                QuestDefineEntities = new List<QuestDefineEntity>()
+                {
+                    new QuestDefineEntity(){Uid=Guid.Empty,   QuestUid=Guid.Empty, QuestionId="Q1", QuestionContent="請輸入您的手機號碼(格式為0912345678，將作為活動通知使用)",NeedAnswer="Y",  AllowNaCondition="",                                                                       AnswerType="F", MinMultipleAnswers=null, MaxMultipleAnswers=null, SingleAnswerCondition="",   CountScoreType="",   Memo="",   OrderSn=10,   CreateUserId="",   CreateTime = null,     ModifyUserId="",    ModifyTime = null, AnswerDefineEntities = new List<AnswerDefineEntity>(){
+                        new AnswerDefineEntity(){AnswerCode=" ", AnswerContent="旅遊行程(例如：來回機票、住宿券)", Memo="", HaveOtherAnswer="Y", NeedOtherAnswer="Y", Score=null, OrderSn=10,},
+                    }   },
+                    new QuestDefineEntity(){Uid=Guid.Empty,   QuestUid=Guid.Empty, QuestionId="Q2", QuestionContent="下列哪種活動贈品最吸引您參加活動?",                     NeedAnswer="Y",  AllowNaCondition="",                                                                       AnswerType="M", MinMultipleAnswers=1,    MaxMultipleAnswers=null, SingleAnswerCondition="",   CountScoreType="",   Memo="",   OrderSn=20,   CreateUserId="",   CreateTime = null,     ModifyUserId="",    ModifyTime = null, AnswerDefineEntities = new List<AnswerDefineEntity>(){
+                        new AnswerDefineEntity(){AnswerCode="A", AnswerContent="3C產品(例如：手機、平板電腦、相機)", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=10,},
+                        new AnswerDefineEntity(){AnswerCode="B", AnswerContent="禮券(例如：知名百貨、量販店、便利商店禮券)", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=20,},
+                        new AnswerDefineEntity(){AnswerCode="C", AnswerContent="簡訊", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=30,}
+                    }   },
+                    new QuestDefineEntity(){Uid=Guid.Empty,   QuestUid=Guid.Empty, QuestionId="Q3", QuestionContent="您最喜歡透過下列何種方式收到行銷優惠資訊?",              NeedAnswer="Y",  AllowNaCondition="",                                                                       AnswerType="M", MinMultipleAnswers=1,    MaxMultipleAnswers=null, SingleAnswerCondition="",   CountScoreType="",   Memo="",   OrderSn=30,   CreateUserId="",   CreateTime = null,     ModifyUserId="",    ModifyTime = null, AnswerDefineEntities = new List<AnswerDefineEntity>(){
+                        new AnswerDefineEntity(){AnswerCode="A", AnswerContent="電子DM", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=10,}                               ,
+                        new AnswerDefineEntity(){AnswerCode="B", AnswerContent="實體DM", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=20,}                               ,
+                        new AnswerDefineEntity(){AnswerCode="C", AnswerContent="即時通訊軟體(例如：LINE、what’s app)", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=30,}    ,
+                        new AnswerDefineEntity(){AnswerCode="D", AnswerContent="其他", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=40,}                                 ,
+                        new AnswerDefineEntity(){AnswerCode="E", AnswerContent="臨櫃", Memo="", HaveOtherAnswer="Y", NeedOtherAnswer="Y", Score=null, OrderSn=50,}                                 ,
+                    }  },
+                    new QuestDefineEntity(){Uid=Guid.Empty,   QuestUid=Guid.Empty, QuestionId="Q4", QuestionContent="下列哪一項通路服務是您最常使用的?(一個月至少使用3次以上)", NeedAnswer="Y",  AllowNaCondition="",                                                                       AnswerType="S", MinMultipleAnswers=null, MaxMultipleAnswers=null, SingleAnswerCondition="",   CountScoreType="",   Memo="",   OrderSn=40,   CreateUserId="",   CreateTime = null,     ModifyUserId="",    ModifyTime = null, AnswerDefineEntities = new List<AnswerDefineEntity>(){
+                        new AnswerDefineEntity(){AnswerCode="A", AnswerContent="ATM", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=10,}       ,
+                        new AnswerDefineEntity(){AnswerCode="B", AnswerContent="網站", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=20,}      ,
+                        new AnswerDefineEntity(){AnswerCode="C", AnswerContent="行動APP", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=30,}   ,
+                        new AnswerDefineEntity(){AnswerCode="D", AnswerContent="都沒有", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=40,}     ,
+                        new AnswerDefineEntity(){AnswerCode="E", AnswerContent="1分", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null,   OrderSn=50,}     ,
+                    }  },
+                    new QuestDefineEntity(){Uid=Guid.Empty,   QuestUid=Guid.Empty, QuestionId="Q5", QuestionContent="您對我們的服務滿意度1至5分是?",                        NeedAnswer="Y",  AllowNaCondition="{\"Conditions\":[{\"QuestionId\": \"Q4\", \"AnswerCode\": [\"E\"]}]}",   AnswerType="S", MinMultipleAnswers=null, MaxMultipleAnswers=null, SingleAnswerCondition="",   CountScoreType="",   Memo="",   OrderSn=50,   CreateUserId="",   CreateTime = null,     ModifyUserId="",    ModifyTime = null, AnswerDefineEntities = new List<AnswerDefineEntity>(){
+                        new AnswerDefineEntity(){AnswerCode="A", AnswerContent="2分", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=10,},
+                        new AnswerDefineEntity(){AnswerCode="B", AnswerContent="3分", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=20,},
+                        new AnswerDefineEntity(){AnswerCode="C", AnswerContent="4分", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=30,},
+                        new AnswerDefineEntity(){AnswerCode="D", AnswerContent="5分", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=40,},
+                        new AnswerDefineEntity(){AnswerCode="E", AnswerContent="", Memo="", HaveOtherAnswer="N", NeedOtherAnswer="N", Score=null, OrderSn=50,}   ,
+                    } },
+
+
+                    new QuestDefineEntity() {Uid=Guid.Parse("F9DD6938-2DBD-48AF-9D44-428DCB2AC5D6"), QuestionId="Q001",    CountScoreType = "2",    NeedAnswer = "N",    AnswerType = "S",     MinMultipleAnswers=null,     MaxMultipleAnswers=null,    SingleAnswerCondition="",                                                                           AllowNaCondition="",                                                                                                                                AnswerDefineEntities = new List<AnswerDefineEntity>(){
+                        new AnswerDefineEntity(){AnswerCode="1", Score = 1, },
+                        new AnswerDefineEntity(){AnswerCode="2", Score = 2, },
+                        new AnswerDefineEntity(){AnswerCode="3", Score = 3, },
+                        new AnswerDefineEntity(){AnswerCode="4", Score = 4, },
+                        new AnswerDefineEntity(){AnswerCode="5", Score = 5, },
+                    } },
+                }
+            };
+
 
             Dictionary<string, string> expected = new Dictionary<string, string>() {
-                { "Q003", "此題僅能勾選1個項目!"},
+                { "Q3", "請輸入其他說明文字!"},
             };
 
             //Actual
