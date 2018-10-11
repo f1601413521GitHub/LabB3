736aa90d7b5d6eb5d856e9f61287198fc6ed08a3
JasonLin <f1601413521@outlook.com>
2018-10-11 10:22:29 +0800

    新增JavaScript主程式檔、調整BundleConfig引用jquery設定。

--- ThinkPower.LabB3.Web/App_Start/BundleConfig.cs
+++ ThinkPower.LabB3.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace ThinkPower.LabB3.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/main.js"));
 
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
