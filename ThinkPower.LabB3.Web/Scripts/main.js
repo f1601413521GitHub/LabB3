85f809768405d3966d8ae7cdbb9acb74b5973bc7
JasonLin <f1601413521@outlook.com>
2018-10-11 11:53:16 +0800

    調整Layout畫面為SinglePageApplication機制、新增main.js方法LoadPartialView。

--- ThinkPower.LabB3.Web/Scripts/main.js
+++ ThinkPower.LabB3.Web/Scripts/main.js
@@ -2,3 +2,20 @@
 $(document).ready(function () {
     console.log('main.js');
 });
+
+function LoadPartialView(type, url, data, appendToElement) {
+
+    $.ajax({
+        type: type,
+        url: url,
+        data: data,
+        cache: false,
+        async: false,
+        success: function (result) {
+            appendToElement.empty().append(result);
+        },
+        error: function () {
+            console.log("系統發生錯誤。");
+        }
+    });
+}
